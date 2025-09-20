35337368d6e1d875c23df7a5382fc2ccee246c0c
boufiis <bufiyassin@gmail.com>
2025-09-20 21:53:34 +0100

    :tada: initial commit

---
+++ AlloJari/AlloJari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlloJari
+//
+//  Created by MAC on 20/9/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AlloJari/AlloJariTests/AlloJariTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlloJariTests.swift
+//  AlloJariTests
+//
+//  Created by MAC on 20/9/2025.
+//
+
+import Testing
+@testable import AlloJari
+
+struct AlloJariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
